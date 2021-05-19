05b160caf82daa0b86ed6c28aed9cf7ceb1a70f0
rtviwe <rtviwe@gmail.com>
2021-05-19 23:52:25 +0300

    add show my location

--- lib/map/content.dart
+++ lib/map/content.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:geolocator/geolocator.dart' as geo;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -15,7 +16,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class MapScreenState extends State<MapScreen> {
-  final Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controllerCompleter = Completer();
+  late GoogleMapController _mapController;
 
   static final CameraPosition initialCameraPos = const CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -27,8 +29,7 @@ class MapScreenState extends State<MapScreen> {
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  final CollectionReference _markers =
-      FirebaseFirestore.instance.collection('markers');
+  final CollectionReference _markers = FirebaseFirestore.instance.collection('markers');
 
   Future<void> addMarker(LatLng location) {
     // Call the user's CollectionReference to add a new user
@@ -61,6 +62,28 @@ class MapScreenState extends State<MapScreen> {
     return Stack(
       children: [
         Scaffold(
+          appBar: AppBar(
+            title: GestureDetector(
+              onTap: () async {
+                try {
+                  final position = await geo.Geolocator.getCurrentPosition(
+                    desiredAccuracy: geo.LocationAccuracy.high,
+                  );
+                  final latLng = LatLng(position.latitude, position.longitude);
+
+                  await _mapController.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        target: latLng,
+                        zoom: 16,
+                      ),
+                    ),
+                  );
+                } on Exception catch (_) {}
+              },
+              child: SvgPicture.asset('assets/svg/ic_gsp.svg'),
+            ),
+          ),
           body: StreamBuilder<QuerySnapshot>(
             stream: _markers.snapshots(),
             builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -71,23 +94,21 @@ class MapScreenState extends State<MapScreen> {
               return GoogleMap(
                 onCameraMove: (CameraPosition cameraPosition) {
                   setState(() {
-                    screenCoords = LatLng(cameraPosition.target.latitude,
-                        cameraPosition.target.longitude);
+                    screenCoords = LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude);
                   });
                 },
                 markers: snapshot.data!.docs.map((DocumentSnapshot document) {
                   var docs = document.data() as Map<String, dynamic>;
                   return Marker(
-                    position: LatLng((docs['LatLng'] as GeoPoint).latitude,
-                        (docs['LatLng'] as GeoPoint).longitude),
+                    position: LatLng((docs['LatLng'] as GeoPoint).latitude, (docs['LatLng'] as GeoPoint).longitude),
                     markerId: MarkerId(document.id),
-                    icon: BitmapDescriptor.defaultMarkerWithHue(
-                        BitmapDescriptor.hueRed),
+                    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
                   );
                 }).toSet(),
                 initialCameraPosition: initialCameraPos,
                 onMapCreated: (GoogleMapController controller) async {
-                  _controller.complete(controller);
+                  _controllerCompleter.complete(controller);
+                  _mapController = controller;
 
                   bool isServiceEnabled;
                   PermissionStatus isPermissionGranted;
